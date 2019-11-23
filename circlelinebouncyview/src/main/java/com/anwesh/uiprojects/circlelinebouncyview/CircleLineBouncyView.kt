2e37a01517f363864329a5010111d0b3cbff506f
Anwesh43 <anweshthecool0@gmail.com>
2019-11-23 18:18:37 +0530

    adding CircleLineBouncy class

--- circlelinebouncyview/src/main/java/com/anwesh/uiprojects/circlelinebouncyview/CircleLineBouncyView.kt
+++ circlelinebouncyview/src/main/java/com/anwesh/uiprojects/circlelinebouncyview/CircleLineBouncyView.kt
@@ -135,7 +135,7 @@ class CircleLineBouncyView(ctx : Context) : View(ctx) {
 
         fun draw(canvas : Canvas, paint : Paint) {
             canvas?.drawCLBNode(i, state.scale, paint)
-            next?.draw(canvas, paint)
+            prev?.draw(canvas, paint)
         }
 
         fun update(cb : (Float) -> Unit) {
@@ -156,6 +156,29 @@ class CircleLineBouncyView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class CircleLineBouncy(var i : Int) {
+
+        private var curr : CLBNode = CLBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
