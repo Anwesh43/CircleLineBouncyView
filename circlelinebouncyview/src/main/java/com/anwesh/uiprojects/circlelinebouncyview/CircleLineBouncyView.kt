f4e9480d49ebc6764f8640089d273a21c08e8f4b
Anwesh43 <anweshthecool0@gmail.com>
2019-11-23 17:59:18 +0530

    adding libraries and constants

--- circlelinebouncyview/src/main/java/com/anwesh/uiprojects/circlelinebouncyview/CircleLineBouncyView.kt
+++ circlelinebouncyview/src/main/java/com/anwesh/uiprojects/circlelinebouncyview/CircleLineBouncyView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.circlelinebouncyview
 /**
  * Created by anweshmishra on 23/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#2196F3")
+val backColor : Int = Color.parseColor("#BDBDBD")
